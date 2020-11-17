bb2ea5f3f8865cd6334d833930fa8cf732d1d8b5
Sean Tiley <sean.tiley@gmail.com>
2020-11-17 08:04:51 -0500

    improve error handling when not allowed to use location

--- src/index.js
+++ src/index.js
@@ -5,22 +5,29 @@ class App extends React.Component{
 
     // not required by react - This is a JS object CTor
     constructor(props){
+        // must call the parent CTOR
         super(props);
         // THIS IS THE ONLY TIME WE DO DIRECT ASSIGNMENT TO OUR STATE!!!!
-        this.state = {lat: null};
+        this.state = {lat: null, errorMessage: ''};
         window.navigator.geolocation.getCurrentPosition(
             (position) => {
                 this.setState({lat: position.coords.latitude})
             },
-            (err) => console.log(err)
+            (err) =>  {
+                console.log(err)
+                this.setState({errorMessage: err.message})
+        }
         );
     }
-
     // react requires we define render that returns JSK
     render(){
-        return (
-            <div>Latitude = {this.state.lat}</div>
-        );
+        if(this.state.errorMessage && !this.state.lat){
+             return <div>Error: {this.state.errorMessage}</div>
+        }
+        if(this.state.lat && !this.state.errorMessage){
+            return <div>Latitude: {this.state.lat}</div>
+        }
+        return <div>loading user location...</div>
     }
 }
 
