6bf75eed1df9794599cce22f211eb5d67fffb6c1
Sean Tiley <sean.tiley@gmail.com>
2020-11-22 14:57:38 -0500

    lecture 71 complete

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,23 +1,37 @@
+import './SeasonDisplay.css';
 import React from "react";
 
+const seasonConfig = {
+    // where the namkes sun and snowflake come from https://semantic-ui.com/elements/icon.html#/icon
+    summer: {
+        text: "Let's hit the beach",
+        iconName: "sun"
+    },
+    winter: {
+        text: "Brrrr it is cold!!!!",
+        iconName: 'snowflake'
+    }
+};
 
 const getSeason=(lat, month) => {
   console.log(`lat=${lat}, month=${month}`);
   if(month >2 && month <9){
-    return lat > 0 ? 'Summer': 'Winter';
+    return lat > 0 ? 'summer': 'winter';
   }
   else{
-    console.log('HERE')
-    return lat > 0 ? 'Winter' : 'Summer';
+    return lat > 0 ? 'winter' : 'summer';
   }
 }
 
   const SeasonDisplay = (props) =>{
     const season = getSeason(props.lat, new Date().getMonth());
-    const message = season === 'Winter' ? 'Brrrr, its chillly': 'Lets hit the beach'
+    //const season = getSeason(props.lat, 7);
+    const {text, iconName} = seasonConfig[season];
     return (
-        <div data-automation='season'>
-          <h2>{message}</h2>
+        <div className={`season-display ${season} data-automation='season'`}>
+          <i className={`icon-left massive ${iconName} icon`}/>
+          <h2>{text}</h2>
+          <i className={`icon-right massive ${iconName} icon `}/>
         </div>
     );
   };
--- src/index.js
+++ src/index.js
@@ -1,35 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinnner";
 
 class App extends React.Component {
     state = {lat: null, errorMessage: ''};
 
-    // react requires we define render that returns JSK
-    render() {
-        console.log("My component rendered")
+    renderContent() {
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>
         }
         if (this.state.lat && !this.state.errorMessage) {
             return <SeasonDisplay lat={this.state.lat}/>
         }
-        return <div>loading user location...</div>
+        return <Spinner loadingMessage="Please allow location request in your browser"/>
     }
-
+    // react requires we define render that returns JSK
+    render() {
+        return (
+            this.renderContent()
+        );
+    }
     componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
             position => this.setState({lat: position.coords.latitude}),
             err => this.setState({errorMessage: err.message})
         );
     }
-
-    componentDidUpdate() {
-        console.log("componentDidUpdate - Called")
-    }
-
-
-
 }
 
 ReactDOM.render(
